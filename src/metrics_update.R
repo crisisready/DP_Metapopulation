3357b702caa091d53a89ccd661375426433a29e1
Koissi <70168615+SaviKoissi@users.noreply.github.com>
2021-04-27 15:28:50 +0200

    R0_Re
    The update version computes the reproduction number for each time step. Of course, if the parameters do not get updated, the metric will give a vector containing the same value.

--- src/metrics_update.R
+++ src/metrics_update.R
@@ -7,12 +7,12 @@ x <- run_seir_model()
 # R0
 R0 <-function(delta = 3.14, alpha = 0.65, mu = 0.5, beta = 0.8){
   R0 = alpha * beta * delta + (1 - alpha) * mu * beta * delta 
-     return(R0)
-     }
+  return(R0)
+}
 
 
 R0_ts <- c()
-num_tsteps <- 60
+num_tsteps = 60
 beta = x[[1]][seq(311, length (x[[1]]), by=316)]
 mu = x[[1]][seq(312, length (x[[1]]), by=316)]
 theta = x[[1]][seq(313, length (x[[1]]), by=316)]
@@ -24,12 +24,10 @@ for (i in 1:num_tsteps){
   R0 = function(...){
     R0 = alpha * beta * delta + (1 - alpha) * mu * beta * delta 
     return(R0)
-    }
+  }
   R0_ts = R0(alpha, beta, delta, mu) 
   return(R0)
 }
- 
+
 R0_ts
-
-
 
