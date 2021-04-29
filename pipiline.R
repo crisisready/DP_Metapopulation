f46f4088d7ef2682e917bb75bb309f7ed7d245bc
Nishant Kishore <nish.kishore@gmail.com>
2021-04-29 12:42:17 -0400

    updated pipeline to reflect most recent changes

--- pipiline.R
+++ pipiline.R
@@ -1,33 +1,6 @@
-# Data aggregation
-require(dplyr)
-require(bigrquery)
+#dependencies
+source("src/dependencies.R")
 
-# Make the query from bigquery
-
-query <- "with tmp as (select activity_day,
-concat(from_state_fips, from_county_fips) as from_fips,
-concat(to_state_fips, to_county_fips) as to_fips, transitions,
-from `hangar-covid-19.camber_covid_aggregations.county_modal_matrix_v4`
-where from_state_fips = '36' and to_state_fips = '36' and activity_day >= '2020-09-01' and activity_day <= '2020-11-01')
-select activity_day, from_fips, to_fips, sum(transitions) as transitions from tmp group by activity_day, from_fips, to_fips"
-
-# save data in a temporal file
-
-data <- bq_dataset_query(x = "hangar-covid-19.camber_covid_aggregations", query = query) %>%
-  bq_table_download(max_results = Inf)
-
-# Overview of the data
-data.cont <- data %>%
-  group_by(count, activity_day) %>%
-  tally()
-movt.larg <- data.cont %>%
-  filter(n >= 10) %>%
-  print()
-
-# Eliminate improbable movements
-
-#  Data winsorizing
-require(DescTools)
-win.data <- data %>%
-  mutate(win.var = Winsorize(data$count, minval = tail(Small(data$count, k = 3), 1), maxval = head(Large(data$count, k = 3), 1))) %>%
-  print() # Here I do not know the reasonable range for a good winsorization
+#model
+run_seir_model() %>%
+  run_metrics()
