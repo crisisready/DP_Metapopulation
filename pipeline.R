b98c31d7bbc6ed8890dc67edccfd44ddae67ed9c
Akash <akash.bsy19@gmail.com>
2021-08-05 11:09:53 -0400

    changed metapopulation.py scripts and passing system args to R scripts

--- pipeline.R
+++ pipeline.R
@@ -1,13 +1,17 @@
 #dependencies
 source("src/dependencies.R")
 
+mechanism <- as.character(commandArgs(TRUE)[2], sep =",")
+epsilon <- as.numeric((as.character(commandArgs(TRUE)[3], sep =",")))
+iterations <- as.numeric((as.character(commandArgs(TRUE)[4], sep =",")))
+
 for(itr_num in 1:as.numeric(iterations)){
   
   seir_matrix <- run_seir_model()
   metrics <- run_metrics(seir_matrix)
   
   #### Saving the output
-  output_directory <- here(sprintf("data/seir-dp-metrics/noise_type=%s/ep=%s/", mechanism, epsilon))
+  output_directory <- here(sprintf("data/seir-dp-metrics/noise_type=%s/ep=%s", mechanism, epsilon))
   
   #### Creating directory if it doesnt exist
   if(!dir.exists(output_directory)){
